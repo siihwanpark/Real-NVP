b60550c00632d59fd6b5c323c91a5667caeaa1b1
psh150204 <psh131226@gmail.com>
2020-08-18 06:04:27 +0000

    update

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import time
+import matplotlib.pyplot as plt
 
 from dataset import ToyDataset
 from torch.utils.data import DataLoader
+from model import RealNVP
 
 dataset = ToyDataset('data.csv')
 
@@ -12,8 +15,40 @@ train_loader = DataLoader(dataset=dataset,
                          shuffle=True, 
                          num_workers=2)
 
-epochs = 1
+epochs = 17
+k = 4
+d = 1
+D = 2
+hidden_dim = 256
+
+prior = torch.distributions.MultivariateNormal(torch.zeros(D), torch.eye(D))
+realNVP = RealNVP(k, d, D, hidden_dim)
+optimizer = torch.optim.Adam(realNVP.parameters(), lr = 0.0001)
 
 for epoch in range(epochs):
     for i, x in enumerate(train_loader):
+        t = time.time()
+        z, log_det_Jacobian = realNVP(x)
+        loss = -(prior.log_prob(z) + log_det_Jacobian).mean()
         
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print("[%d/%d][%d/%d] loss : %.4f | time :%.2fs"%(epoch+1, epochs, i+1, len(train_loader), loss.item(), time.time()-t))
+
+
+latent = []
+for i, x in enumerate(train_loader):
+    z, _ = realNVP(x)
+    latent.append(z.detach().numpy())
+
+z = np.concatenate(latent, axis = 0)
+
+fig, ax = plt.subplots()
+ax.scatter(z[:,0], z[:,1])
+ax.set(title="z = f(X)")
+ax.grid()
+
+fig.savefig("f_x.png")
+plt.close()
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class AffineCouplingLayer(nn.Module):
     def __init__(self, d, D, hidden_dim):
@@ -14,21 +15,23 @@ class AffineCouplingLayer(nn.Module):
         self.layers = nn.Sequential(
             nn.Linear(d, hidden_dim),
             nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
             nn.Linear(hidden_dim, 2 * (D-d))
         )
         
     def forward(self, x_a, x_b):
         output = self.layers(x_a) # [batch, 2 * (D-d)]
-        log_s, t = output[:, :self.D-self.d], output[:, self.D-self.d:]
+        log_s, t = F.tanh(output[:, :self.D-self.d]), output[:, self.D-self.d:]
 
         z_a = x_a # [batch, d]
         z_b = torch.exp(log_s) * x_b + t # [batch, D-d]
 
-        return z_a, z_b
+        return z_a, z_b, log_s
 
     def backward(self, z_a, z_b):
         output = self.layers(z_a)
-        log_s, t = output[:, :self.D-self.d], output[:, self.D-self.d:]
+        log_s, t = F.tanh(output[:, :self.D-self.d]), output[:, self.D-self.d:]
 
         x_a = z_a
         x_b = torch.exp(-log_s) * (z_b - t)
@@ -36,12 +39,24 @@ class AffineCouplingLayer(nn.Module):
         return x_a, x_b
 
 class RealNVP(nn.Module):
-    def __init__(self, d, D, hidden_dim):
+    def __init__(self, k, d, D, hidden_dim):
+        super(RealNVP, self).__init__()
+        self.k = k
         self.d = d
         self.D = D
-        self.layers = nn.ModuleList(AffineCouplingLayer(d, D, hidden_dim) for _ in range(4))
+        self.layers = nn.ModuleList(AffineCouplingLayer(d, D, hidden_dim) for _ in range(k))
 
     def forward(self, x):
         x_a, x_b = x[:, :self.d], x[:, self.d:]
-        h1_a, h1_b = self.layers[0](x_a, x_b)
-        h2_a, h2_b = self.layers[0](h1_b, h1_a)
+
+        h_a, h_b = x_a, x_b
+        log_det_Jacobian = 0
+
+        for i in range(self.k):
+            if i % 2 == 0:
+                h_a, h_b, log_s = self.layers[i](h_a, h_b)
+            else:
+                h_a, h_b, log_s = self.layers[i](h_b, h_a)
+            log_det_Jacobian += log_s.sum()
+
+        return torch.cat([h_a, h_b], dim = -1), log_det_Jacobian
